54e8c69cd510561d0611696d9fc6752fb6201d0b
Rioo <ruoilin7@gmail.com>
2025-09-30 13:02:10 +0800

    Add files via upload

---
+++ plugins/ai-chat/chatgpt.js
@@ -0,0 +1,23 @@
+import gpt from '#scrape/chatgpt.js';
+
+export default {
+name: "chatgpt",
+category: "ai-chat",
+command: ["chatgpt", "gpt"],
+run: async (conn, m) => {
+const input = m.isQuoted ? m.quoted.text : m.text;
+if (!input) return m.reply(`Masukkan pertanyaan atau perintah!\n\nContoh:\n${m.cmd} apa itu AI`);
+
+if (!conn.chatgpt) conn.chatgpt = {};
+if (!conn.chatgpt[m.sender]) conn.chatgpt[m.sender] = [];
+conn.chatgpt[m.sender].push({ role: "user", content: input });
+try {
+const res = await gpt("gpt-4o-nano", conn.chatgpt[m.sender]);
+
+conn.chatgpt[m.sender].push({ role: "assistant", content: res });
+m.reply(res)
+} catch (err) {
+console.error(err);
+}
+}
+};
---
+++ plugins/ai-chat/deepseek.js
@@ -0,0 +1,28 @@
+import deepseek from '#scrape/deepseek.js';
+
+export default {
+name: "deepseek",
+category: "ai-chat",
+command: ["deepseek", "dipsek"],
+run: async (conn, m) => {
+const input = m.isQuoted ? m.quoted.text : input;
+if (!input) return m.reply(`Masukkan pertanyaan atau perintah!\n\nContoh:\n${m.cmd} apa itu AI`);
+
+if (!conn.deepseek) conn.deepseek = {};
+if (!conn.deepseek[m.sender]) conn.deepseek[m.sender] = [];
+conn.deepseek[m.sender].push({ content: input, is_user: true });
+
+try {
+const res = await deepseek(input, conn.deepseek[m.sender]);
+const result = res.replace(/<think>[\s\S]*?<\/think>/gi, '')
+.replace(/&quot;/g, '"')
+.replace(/<br\s*\/?>/gi, '\n')
+.replace(/&#039;/g, "'")
+.trim();
+conn.deepseek[m.sender].push({ content: res, is_user: false });
+m.reply(result)
+} catch (err) {
+console.error(err);
+}
+}
+};
