e0009252220a9c781b4454f1879bc0529931261a
Rioo <ruoilin7@gmail.com>
2025-09-30 13:00:32 +0800

    Add files via upload

---
+++ plugins/owner/sfp.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+export default {
+name: "sfp",
+category: "owner",
+command: ["sfp", "safeplugin"],
+settings: {
+owner: true
+},
+run: async (conn, m, { quoted }) => {
+if (!m.text) return m.reply(`Nama filenya??\nContoh: ${m.cmd} folder/namafile`);
+if (!quoted.body) return m.reply(`balas pesan nya!`)
+let path = './plugins/' + m.text + '.js'
+fs.writeFileSync(path, quoted.body)
+m.reply(`tersimpan di ${path}`)
+}
+}
